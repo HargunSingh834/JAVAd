b1fb96d3d4b9a0260fd5eb18de1948202ae00fa5
Hargunsingh <hargunsingh834@gmail.com>
2025-08-01 23:39:07 +0530

    commit hh

---
+++ Controlflow/loops05.js
@@ -0,0 +1,12 @@
+//for each loop
+const coding=["js","python","cpp","java","C++"]
+
+// coding.forEach( function(value) {    -------------->for each loop
+//     console.log(value);
+// })
+
+// coding.forEach( (item) => {    //arrow funct
+//     console.log(item);
+    
+// })
+
